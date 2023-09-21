76fba7e686c0e264f84af94ac30137783ee144d1
gabriele.monti24 <gabriele.monti@we-plus.eu>
2023-09-21 15:23:32 +0200

    Cambiato test 1 in test2

--- SandBoxGiuseppe/Controllers/TestController.cs
+++ SandBoxGiuseppe/Controllers/TestController.cs
@@ -19,7 +19,7 @@ namespace SandBoxGiuseppe.Controllers
         public IActionResult Get()
         {
             //commento
-            return Ok("Test1");
+            return Ok("Test2");
         }
 
         [HttpGet]
